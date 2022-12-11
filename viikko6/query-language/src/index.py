924caceacbb4bae1a2fd9c2a8bf9e12867d3d537
Ahannila <arttuhannila123@gmail.com>
2022-12-11 16:27:33 +0200

    Lisätty ja testattu että luokat Not, HasFewerThan ja All toimivat tiedostossa matchers.py ja index.py

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,17 +1,30 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, HasFewerThan, Not
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    #matcher = And(
+    #    HasAtLeast(5, "goals"),
+    #    HasAtLeast(5, "assists"),
+    #    PlaysIn("PHI")
+    #)
+    
+    #matcher = And(
+    #    HasFewerThan(1, "goals"),
+    #    PlaysIn("NYR")
+    #)
+
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
     )
+
+
+    
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,5 +1,5 @@
 class And:
-    def __init__(self, *matchers):
+    def __init__(self, *matchers):  #Syntaksin avulla *matchers sisältää listan konstruktorille annetuista argumenteista
         self._matchers = matchers
 
     def test(self, player):
@@ -12,15 +12,27 @@ class And:
 class All:
     def __init__(self) -> None:
         pass
+    
+    def test(self):
+        return True
 
 class Not:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, condition):
+        self._cond = condition
+    
+    def test(self, player):
+        return self._cond.test(player) is not True
 
 class HasFewerThan:
-    def __init__(self) -> None:
-        pass
-    
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
